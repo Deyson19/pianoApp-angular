fea23c2388bb28fc5de0e8c2f51c837fc4df1409
Deyson Venté <deiandresvent@hotmail.com>
2024-04-08 16:29:14 -0500

    Piano App - Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
 
+interface Sonidos {
+  id: number;
+  nombre: string;
+  styleClass?: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pianoApp';
+  currentUrl = '../assets/sonidos/';
+  public publicClass = 'list-group-item list-group-item-action';
+  public pianoSounds: Sonidos[] = [
+    {
+      id: 1,
+      nombre: 'Sonido 1',
+      styleClass: this.publicClass + 'list-group-item-primary bg-primary',
+    },
+    {
+      id: 2,
+      nombre: 'Sonido 2',
+      styleClass: this.publicClass + 'list-group-item-success bg-success',
+    },
+    {
+      id: 3,
+      nombre: 'Sonido 3',
+      styleClass: this.publicClass + 'list-group-item-danger bg-danger',
+    },
+    {
+      id: 4,
+      nombre: 'Sonido 4',
+      styleClass: this.publicClass + 'list-group-item-warning bg-warning',
+    },
+    {
+      id: 5,
+      nombre: 'Sonido 5',
+      styleClass: this.publicClass + 'list-group-item-dark bg-primary-dark',
+    },
+  ];
+
+  playThePiano(id: number) {
+    const audio = new Audio();
+    audio.src = this.currentUrl + id + '.mp3';
+    audio.volume = 0.1;
+    audio.load();
+    audio.play();
+  }
 }
